72216c06b77f0fbe7ef969abad7548269f93ba67
Daneris Mendoza <danerismendoza096@gmail.com>
2024-03-09 10:26:14 +0800

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <v-row class="d-flex  flex-column align-center justify-center ">
+        <div class="text-h1">Daneris Mendoza</div>
+        <div class="text-h3 font-weight-light" style="color: gray;">FULL STACK DEVELOPER</div>
+      </v-row>
+    </v-responsive>
+    <v-responsive class="align-center text-center fill-height">
+      <v-row class="d-flex  flex-column align-center justify-center ">
+        <div class="text-h1">PROJECTS</div>
+        <div class="text-h3 font-weight-light" style="color: gray;">FULL STACK DEVELOPER</div>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+</script>
+
+<style>
+section {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+}
+</style>
---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app-bar flat color="#000000">
+    <!-- <v-icon icon="mdi-circle-slice-4" /> -->
+    <v-app-bar-title style="display: flex;  justify-content: center; " class="ml-12">
+      <v-btn>
+        HOME
+      </v-btn>
+
+      <v-btn>
+        PORTFOLIO
+      </v-btn>
+
+      <v-btn>
+        SKILLS
+      </v-btn>
+
+      <v-btn>
+        CONTACT
+      </v-btn>
+
+    </v-app-bar-title>
+    <template v-slot:append>
+      <v-switch label="Dark" inset class="mt-5"></v-switch>
+    </template>
+  </v-app-bar>
+</template>
+
+<script setup>
+//
+</script>
