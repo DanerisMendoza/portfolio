a879a6cbef3d4a2ca5bf77f18444fcf255c48373
Daneris Mendoza <danerismendoza096@gmail.com>
2024-03-22 18:01:47 +0800

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,50 @@
 <template>
-  <v-container class="fill-height">
-    <v-responsive class="align-center text-center fill-height">
-      <v-row class="d-flex  flex-column align-center justify-center ">
-        <div class="text-h1">Daneris Mendoza</div>
-        <div class="text-h3 font-weight-light" style="color: gray;">FULL STACK DEVELOPER</div>
-      </v-row>
-    </v-responsive>
-    <v-responsive class="align-center text-center fill-height">
-      <v-row class="d-flex  flex-column align-center justify-center ">
-        <div class="text-h1">PROJECTS</div>
-        <div class="text-h3 font-weight-light" style="color: gray;">FULL STACK DEVELOPER</div>
-      </v-row>
-    </v-responsive>
-  </v-container>
+  <section id="section1">
+    <div class="text-h1">Daneris Mendoza</div>
+    <div class="text-h3 font-weight-light" style="color: gray;">FULL STACK DEVELOPER</div>
+  </section>
+  <section id="section2" style="background-color: lightgrey;">
+    <div class="text-h3 mt-6" style="color: black;">PROJECTS</div>
+    <div class="card-container">
+
+      <v-card width="500" height="650" density="comforable" elevation="20" outlined style="justify-content: center" class="pt-2 pl-2">
+        <v-img class="align-end text-white" height="350" src="../assets/system.png" >
+        </v-img>
+        <v-card-title class="d-flex justify-center">E-Palengke</v-card-title>
+        <v-card-text>
+
+          <strong>Stacks: </strong>
+          <v-chip>Vue.2(Web)</v-chip>
+          <v-chip>Vue.3(Mobile)</v-chip>
+          <v-chip>Ionic</v-chip>
+          <v-chip>Laravel.10</v-chip>
+          <br>
+          <strong>Platform: </strong>
+          <v-chip>Web</v-chip>
+          <v-chip>Mobile</v-chip>
+          <br>
+          <strong>Type: </strong>
+          <v-chip>Academic Thesis</v-chip>
+        </v-card-text>
+
+        <v-card-subtitle>Web Based Ecomerce</v-card-subtitle>
+        <v-card-subtitle>Multiple user role (Admin, Customer, Seller and Delivery)</v-card-subtitle>
+
+        <v-card-actions  class="d-flex justify-center">
+          <v-btn color="black" variant="outlined">
+            View Website
+          </v-btn>
+
+          <v-btn color="black" variant="outlined">
+            More Details
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+
+
+    </div>
+
+  </section>
 </template>
 
 <script setup>
@@ -22,7 +54,33 @@
 section {
   display: flex;
   flex-direction: column;
+  align-items: center;
   width: 100%;
   height: 100vh;
+}
+
+#section1 {
+  justify-content: center;
+}
+
+#section2 {
+  /* justify-content: center; */
+}
+
+.card-container {
+  display: flex;
+  height: 100%;
+  align-items: center;
+  flex-direction: row;
+}
+
+
+
+.v-card {
+  margin: 1.5rem;
+}
+
+.v-chip {
+  margin: 0.2rem;
 }
 </style>
--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,12 +1,11 @@
 <template>
-  <v-app-bar flat color="#000000">
-    <!-- <v-icon icon="mdi-circle-slice-4" /> -->
-    <v-app-bar-title style="display: flex;  justify-content: center; " class="ml-12">
-      <v-btn>
+ <v-app-bar flat color="transparent">
+    <!-- <v-app-bar-title style="display: flex;  justify-content: center; " class="ml-12">
+      <v-btn href="#section1">
         HOME
       </v-btn>
 
-      <v-btn>
+      <v-btn href="#section2">
         PORTFOLIO
       </v-btn>
 
@@ -18,7 +17,7 @@
         CONTACT
       </v-btn>
 
-    </v-app-bar-title>
+    </v-app-bar-title> -->
     <template v-slot:append>
       <v-switch label="Dark" inset class="mt-5"></v-switch>
     </template>
